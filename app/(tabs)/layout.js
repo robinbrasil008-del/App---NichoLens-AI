9d29ea106a71758c35384490e06f029b3dc4c821
robinbrasil008-del <robinbrasil008@gmail.com>
2026-02-03 02:24:44 -0300

    Update layout.js

--- app/(tabs)/layout.js
+++ app/(tabs)/layout.js
@@ -1,33 +1,11 @@
 "use client";
 
-export default function TabsLayout({ children }) {
+export default function Layout({ children }) {
   return (
-    <div style={{ minHeight: "100vh", paddingBottom: 70 }}>
-      {children}
-
-      <nav
-        style={{
-          position: "fixed",
-          bottom: 0,
-          left: 0,
-          right: 0,
-          height: 60,
-          background: "#ffffff",
-          borderTop: "1px solid #e5e5e5",
-          display: "flex",
-          justifyContent: "space-around",
-          alignItems: "center",
-          zIndex: 1000
-        }}
-      >
-        <a href="/" style={{ textDecoration: "none", color: "#111" }}>
-          🏠 <br /> Início
-        </a>
-
-        <a href="/chat" style={{ textDecoration: "none", color: "#111" }}>
-          💬 <br /> Chat
-        </a>
-      </nav>
-    </div>
+    <html>
+      <body style={{ margin: 0 }}>
+        {children}
+      </body>
+    </html>
   );
 }
