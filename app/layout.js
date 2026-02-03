883b3d29a6f5c6b0a3a2040454c08f7c89ff902e
robinbrasil008-del <robinbrasil008@gmail.com>
2026-02-02 23:25:10 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
