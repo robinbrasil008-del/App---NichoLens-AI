9444a5d4454d280dc4323bfa90199167c0de505f
robinbrasil008-del <robinbrasil008@gmail.com>
2026-02-05 20:46:36 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,17 @@
 "use client";
 
 import { TicketProvider } from "./context/TicketContext";
+import { SessionProvider } from "next-auth/react";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <body style={{ margin: 0 }}>
-        <TicketProvider>
-          {children}
-        </TicketProvider>
+        <SessionProvider>
+          <TicketProvider>
+            {children}
+          </TicketProvider>
+        </SessionProvider>
       </body>
     </html>
   );
